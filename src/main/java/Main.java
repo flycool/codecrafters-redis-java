2afe9e72fdb0624ccc98b9c44037e367df0786e1
flycool <flycoolcool@163.com>
2026-07-13 16:41:59 +0800

    deserialize commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,73 +6,19 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import components.TcpServer;
+
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible
-    // when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible
+        // when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
-    try {
-      serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-      while (true) {
-        clientSocket = serverSocket.accept();
-        Socket finalClientSocket = clientSocket;
-        CompletableFuture.runAsync(() -> {
-          handleClient(finalClientSocket);
-        });
-
-      }
-
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
+        TcpServer app = context.getBean(TcpServer.class);
+        app.startServer();
     }
-  }
-
-  public static void handleClient(Socket clientSocket) {
-    try {
-      InputStream inputStream = clientSocket.getInputStream();
-      OutputStream outputStream = clientSocket.getOutputStream();
-      Scanner sc = new Scanner(inputStream);
-
-      while (sc.hasNextLine()) {
-        String nextLine = sc.nextLine();
-        if (nextLine.contains("PING")) {
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-        }
-        if (nextLine.contains("ECHO")) {
-          String respHeader = sc.nextLine();
-          String respBody = sc.nextLine();
-          String resp = respHeader + "\r\n" + respBody + "\r\n";
-          outputStream.write(resp.getBytes());
-        }
-      }
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    }
-  }
-
-  public static String encodingRespString(String s) {
-    int len = s.length();
-    String resp = "$";
-    resp += len;
-    resp += "\r\n" + s + "\r\n";
-    return resp;
-  }
-
 }
