589a2dfcc4b60ef49bb57e17939e0ffd6f19fa87
flycool <flycoolcool@163.com>
2026-07-17 13:10:14 +0800

    set value for expiry

--- src/main/java/components/RespSerializer.java
+++ src/main/java/components/RespSerializer.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +27,8 @@ public class RespSerializer {
 //        String test = """
 //                *2\r\n*3\r\n$3\r\nSET\r\n$5\r\nmango\r\n$9\r\nblueberry\r\n*3\r\n$3\r\nSET\r\n$5\r\nmango\r\n$9\r\nblueberry\r\n
 //                """;
-        String test = "*2\r\n*3\r\n$3\r\nSET\r\n$5\r\nmango\r\n$9\r\nblueberry\r\n*2\r\n$3\r\nGET\r\n$5\r\nmango";
+//        String test = "*2\r\n*3\r\n$3\r\nSET\r\n$5\r\nmango\r\n$9\r\nblueberry\r\n*2\r\n$3\r\nGET\r\n$5\r\nmango";
+        String test = "*2\r\n*3\r\n$3\r\nSET\r\n$5\r\nmango\r\n$9\r\nblueberry\r\n$2px\r\n$3100\r\n*2\r\n$3\r\nGET\r\n$5\r\nmango";
 //        String test = "*1\r\n$4PING";
 //        String test = "*1\r\n$4ECHO$3max";
         List<ArrayList<String>> res = serializer.deserialize(test.getBytes(StandardCharsets.UTF_8));
@@ -61,6 +63,11 @@ public class RespSerializer {
 
             ArrayList<String> commandList = new ArrayList<>();
             for (int j = 1; j < cc.length; j++) {
+                if(isStringDigit(cc[j])) {
+                    String s = cc[j].substring(1);
+                    commandList.add(s);
+                    continue;
+                }
                 String c = removeNumFromString(cc[j]);
                 commandList.add(c);
             }
@@ -77,36 +84,35 @@ public class RespSerializer {
             if (command.contains("PING")) {
                 System.out.println("+PONG\r\n");
                 //client.outputStream.write("+PONG\r\n".getBytes());
-            }
-
-            for (int i = 1; i < c.size(); i++) {
-                if (command.contains("ECHO")) {
-                    String resp = encodingRespString(c.get(i));
-                    System.out.println(resp);
-                    //client.outputStream.write(resp.getBytes());
-                    break;
-                } else if (command.equals("SET")) {
-                    String key = c.get(i);
-                    String value = c.get(i + 1);
-                    System.out.println("key: " + key);
-                    System.out.println("value: " + value);
-
-                    String ok = store.set(key, value);
-                    System.out.println("ok: " + ok);
-                    break;
-                } else if (command.equals("GET")) {
-                    String key = c.get(i);
-                    System.out.println("v: " + key);
-
-                    String value = store.get(key);
-                    System.out.println("value: " + value);
-                    break;
+            } else if (command.contains("ECHO")) {
+                String resp = encodingRespString(c.get(1));
+                System.out.println(resp);
+            } else if (command.equals("SET")) {
+                long expiry = -1;
+                int pxIndex = c.indexOf("px");
+                if (pxIndex != -1) {
+                    expiry = Long.parseLong(c.get(pxIndex + 1));
                 }
+
+                String key = c.get(1);
+                String value = c.get(2);
+                System.out.println("key: " + key);
+                System.out.println("value: " + value);
+
+                String ok = store.set(key, value, expiry);
+                System.out.println("ok: " + ok);
+            } else if (command.equals("GET")) {
+                String key = c.get(1);
+                System.out.println("v: " + key);
+
+                String value = store.get(key);
+                System.out.println("value: " + value);
             }
+
         }
     }
 
-    // 3max, 12abcdefghijkl
+    // 3max, 12abcdefghijkl 3100
     private String removeNumFromString(String str) {
         int i = 0;
         while (i < str.length()) {
@@ -116,6 +122,15 @@ public class RespSerializer {
             i++;
         }
         return str.substring(i);
+    }
+
+    private boolean isStringDigit(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static String encodingRespString(String s) {
--- src/main/java/components/Store.java
+++ src/main/java/components/Store.java
@@ -3,6 +3,7 @@ package components;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -18,9 +19,15 @@ public class Store {
         return map.keySet();
     }
 
-    public String set(String key, String v) {
+    // expiry = -1 : no expiry
+    public String set(String key, String v, long expiryMills) {
         try {
-            Value value = new Value(v, LocalDateTime.now(), LocalDateTime.MAX);
+            LocalDateTime now = LocalDateTime.now();
+            LocalDateTime expire = LocalDateTime.MAX;
+            if (expiryMills != -1) {
+                expire = now.plus(expiryMills, ChronoUnit.MILLIS);
+            }
+            Value value = new Value(v, now, expire);
             map.put(key, value);
             return "+OK\r\n";
         } catch (Exception e) {
@@ -32,6 +39,12 @@ public class Store {
     public String get(String key) {
         try {
             Value value = map.get(key);
+            LocalDateTime now = LocalDateTime.now();
+            LocalDateTime expire = value.expiry;
+            if(expire.isBefore(now)) {
+                map.remove(key);
+                return "$-1\r\n";
+            }
             return value.value;
         } catch (Exception e) {
             System.out.println(e.getMessage());
